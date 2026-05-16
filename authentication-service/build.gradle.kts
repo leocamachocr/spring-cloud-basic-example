92f02f55c06560782a5116c332af753c89c193d8
Leonardo Camacho <leocamachocr@gmail.com>
2026-05-16 07:38:44 -0600

    adding migration stufss for zipking spring boot

--- authentication-service/build.gradle.kts
+++ authentication-service/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "4.0.5"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -9,14 +9,14 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2025.1.1"
+extra["springCloudVersion"] = "2024.0.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -27,8 +27,9 @@ dependencies {
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
     implementation("io.zipkin.reporter2:zipkin-reporter-brave")
     runtimeOnly("com.h2database:h2")
-    implementation("org.springframework.security:spring-security-crypto")
+    implementation("org.springframework.security:spring-security-crypto:6.2.2")
     implementation("io.jsonwebtoken:jjwt:0.12.5")
+    implementation("javax.xml.bind:jaxb-api:2.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
 }
